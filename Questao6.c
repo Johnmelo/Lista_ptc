d46c3233d2094b1b91272f8b38a6697f2c58e8f1
Jonathas <Jonathas_ruam@hotmail.com>
2013-05-26 22:55:34 -0300

    Update Questao6.c

--- Questao6.c
+++ Questao6.c
@@ -3,14 +3,14 @@ struct Pessoa{
   char* nome;
   char* sexo;
   char* estado_civil;
-  char* idade;
+  int idade;
 } ;
 int main(){
   struct Pessoa pessoa;
   pessoa.nome="Jonathas";
   pessoa.sexo="Masculino"; 
   pessoa.estado_civil="Solteiro";
-  pessoa.idade="20 anos";
-  printf("NOME: %s\nSEXO: %s\nESTADO CIVIL: %s\nIDADE:  %s\n",pessoa.nome,pessoa.sexo,pessoa.estado_civil,pessoa.idade);
+  pessoa.idade= 20;
+  printf("NOME: %s\nSEXO: %s\nESTADO CIVIL: %s\nIDADE:  %i\n",pessoa.nome,pessoa.sexo,pessoa.estado_civil,pessoa.idade);
   return 0;
 }
